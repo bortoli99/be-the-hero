3c5b16973796c0fb935777459cd737f8fedcad6b
bruno bortoli <bruno.bortoli25@gmail.com>
2020-03-26 13:15:00 -0500

    Front

--- day1/src/controllers/IncidentController.js
+++ day1/src/controllers/IncidentController.js
@@ -7,12 +7,12 @@ module.exports = {
         const [count] = await connection('incidents').count();
 
         const incidents = await connection('incidents')
-        .join('ongs', 'ongs.id', '=', 'incidents.ongs_id')
+        .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
         .limit(5)
         .offset((page - 1) * 5)
         .select(['incidents.*', 'ongs.name', 'ongs.email', 'ongs.whatsapp', 'ongs.city', 'ongs.uf']);
 
-        response.headers('X-Total-Count', count['count(*)']);
+        response.header ('X-Total-Count', count['count(*)']);
     
         return response.json(incidents);
     },
